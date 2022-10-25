bcb92617f1df30eae16574573191a6f87cb9f796
C7Lemon <cedric.paetzold@itc-studenten.de>
2022-10-25 15:06:15 +0200

    HTML, CSS fertig? Teilelweise JS, nur Anzeige und Anzeigeleeren

---
+++ TaschenrechnerJS.js
@@ -0,0 +1,19 @@
+
+
+function dis(val){
+    document.getElementById("disp").value+=val
+}
+function clr(){
+    document.getElementById("disp").value = ""
+}
+
+
+
+
+/*function clear() {
+    document.getElementById("output").innerHTML = "0";
+}
+function fordisplay(value) {
+    //removeZero()
+    document.getElementById("output").innerHTML += value;
+}*/
