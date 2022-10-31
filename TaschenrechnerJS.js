d191ebfa7157ce914bec1330ae5e45bcebc97e58
C7Lemon <cedric.paetzold@itc-studenten.de>
2022-10-31 14:54:03 +0100

    HTML, CSS fertig? JS funktioniert eigentlich alles, aber würde gerne JS ändern

--- TaschenrechnerJS.js
+++ TaschenrechnerJS.js
@@ -3,11 +3,14 @@
 function dis(val){
     document.getElementById("disp").value+=val
 }
+function solve(){
+    let x = document.getElementById("disp").value
+    let y = eval(x)
+    document.getElementById("disp").value = y
+}
 function clr(){
     document.getElementById("disp").value = ""
 }
-
-
 
 
 /*function clear() {
